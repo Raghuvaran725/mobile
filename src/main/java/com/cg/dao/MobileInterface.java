cab2c4e01fc3256227ac85f9fbf0b8b8d1ef0e28
Raghuvaran725 <kraghuvaran7097@gmail.com>
2023-10-20 13:23:38 +0530

    dao class commit

---
+++ src/main/java/com/cg/dao/MobileInterface.java
@@ -0,0 +1,14 @@
+package com.cg.dao;
+
+import java.util.List;
+
+import com.cg.entity.Mobile;
+
+public interface MobileInterface {
+
+	public String addLaptop(Mobile mobile);
+
+		public List<Mobile> getallMobiles();
+	
+	
+}
